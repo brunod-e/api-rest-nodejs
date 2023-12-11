761719806704b92234507c2b2153ab11db622727
Bruno <brunodaniel.elias@gmail.com>
2023-12-11 16:02:44 -0300

    feat: get all and get single transaction routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,23 @@ import { knex } from "../database"
 import { randomUUID } from "node:crypto"
 
 export async function transactionRoutes(server: FastifyInstance) {
+  server.get("/", async () => {
+    const transactions = await knex("transactions").select("*")
+
+    return { transactions }
+  })
+
+  server.get("/:id", async (req) => {
+    const getTransactionSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionSchema.parse(req.params)
+
+    const transaction = await knex("transactions").where({ id }).first()
+
+    return { transaction }
+  })
+
   server.post("/", async (req, reply) => {
     const createTransactionSchema = z.object({
       title: z.string(),
